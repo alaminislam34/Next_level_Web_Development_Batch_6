7164121d4a0548f543dee2aab3d04adf67e80c5d
alaminislam34 <mi3548514@gmail.com>
2025-11-06 00:26:38 +0600

    practice video 02

---
+++ Mission_1/Module_4/practice02.js
@@ -0,0 +1,35 @@
+// Mutual friends counter with Set
+
+const UUSER_COUNT = 50000;
+let usersA = [];
+let usersB = [];
+
+const createUser = (id) => ({ id: `user_${id}`, name: `User ${id}` });
+
+for (let i = 0; i < UUSER_COUNT; i++) {
+  usersA.push(createUser(i));
+  usersB.push(createUser(i + 25000));
+}
+
+const GetMutualFriends = (usersA, usersB) => {
+  const mutualFriends = [];
+  const start = performance.now();
+
+  //   ! O(n^2)
+  //   usersA.forEach((userA) => {
+  //     usersB.forEach((userB) => {
+  //       if (userB.id === userA.id) {
+  //         mutualFriends.push(userA);
+  //       }
+  //     });
+  //   });
+  const idListed = new Set(usersA.map((user) => user.id));
+  usersA.forEach((userA) => {
+    if (idListed.has(userA.id)) {
+      mutualFriends.push(userA);
+    }
+  });
+  const end = performance.now();
+  return { commonFriend: mutualFriends.length, takeTime: end - start };
+};
+console.log(GetMutualFriends(usersA, usersB));
