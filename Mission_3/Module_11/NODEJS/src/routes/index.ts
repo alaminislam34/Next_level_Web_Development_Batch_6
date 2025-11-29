d3ee5aee319914a6368809487117dad2080a34ab
alaminislam34 <mi3548514@gmail.com>
2025-11-29 23:26:54 +0600

    practice node .js server

--- Mission_3/Module_11/NODEJS/src/routes/index.ts
+++ Mission_3/Module_11/NODEJS/src/routes/index.ts
@@ -1,3 +1,4 @@
+import { readFiles, writeFile } from "../helper/fileDb";
 import parseBody from "../helper/parseBody";
 import addRoutes from "../helper/RouteHanlder";
 import sendJson from "../helper/send-json";
@@ -10,7 +11,25 @@ addRoutes("GET", "/api", (req, res) => {
   sendJson(res, 200, { message: "Health status ok", path: req.url });
 });
 
+addRoutes("GET", "/api/users", (req, res) => {
+  const users = readFiles();
+  sendJson(res, 200, users);
+});
+
 addRoutes("POST", "/api/create-user", async (req, res) => {
   const body = await parseBody(req);
-  sendJson(res, 201, { success: true, data: body });
+
+  let users = readFiles();
+  if (!Array.isArray(users)) users = []; // safety check
+
+  const newUser = {
+    id: Date.now(),
+    ...body,
+  };
+
+  users.push(newUser);
+
+  writeFile(users); // <-- send whole array now
+
+  sendJson(res, 201, { success: true, data: newUser });
 });
