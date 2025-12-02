994a95a811a156db3d5a90cdcfdb58da459013ed
alaminislam34 <mi3548514@gmail.com>
2025-12-02 16:13:30 +0600

    added some route

--- Mission_3/Module_11/NODEJS/src/server.ts
+++ Mission_3/Module_11/NODEJS/src/server.ts
@@ -1,4 +1,5 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
@@ -12,9 +13,36 @@ const server: Server = http.createServer(
         })
       );
     }
+
+    if (req.url == "/api" && req.method == "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Health status ok",
+          path: req.url,
+        })
+      );
+    }
+
+    if (req.url == "/api/create-user" && req.method == "POST") {
+      res.writeHead(201, { "content-type": "application/json" });
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        try {
+          const parseBody = JSON.parse(body);
+          console.log(parseBody);
+          res.end(JSON.stringify(parseBody));
+        } catch (err: any) {
+          console.log(err?.message);
+        }
+      });
+    }
   }
 );
 
-server.listen(5000, () => {
-  console.log("Server is running on port:", 5000);
+server.listen(config.port, () => {
+  console.log("Server is running on port:", config.port);
 });
